d3c30203291e04676d462048e815ac9d05fe3d24
BradleyGCF <bradleycarucci@gmail.com>
2025-01-30 20:20:31 -0400

    add: getElementById

---
+++ metodos.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 4, 5];
+const result = numbers.map((number) => number * 10);
+console.log(result); // [10, 20, 30, 40, 50]
+
+//Ejercicio 2: Tienes un array de objetos que representan productos con propiedades nombre y precio. Usa map() para crear un nuevo array con los precios aumentados en un 20%.0
+const products = [
+  { name: "product1", price: 100 },
+  { name: "product2", price: 150 },
+];
---
+++ topping-de-pizza/app.js
@@ -0,0 +1,4 @@
+const titulo = document.getElementById("titulo");
+console.log(titulo.tagName);
+
+// En la clase de ma;ana debo continuar con .getElementByClassName en el minuto 1:14:54 en youtube
